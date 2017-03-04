fa2d20a3b1d65d31582edf66d65d3ed54f21cbfc
Jiabao Li <stephlijiabao@gmail.com>
2017-03-04 16:57:51 -0500

    Hololens

---
+++ Particle System/Assets/Main.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+    //public GameObject Butterfly;
+    // Use this for initialization
+    GameObject world;
+    void Start () {
+        world = GameObject.Find("world");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.transform.position.x < 1.5 && this.transform.position.x > 0.5 && this.transform.position.z < 1.5 && this.transform.position.z > 0.5)
+         {
+            Debug.Log("world transform");
+            world.transform.position = new Vector3(1, 0.2f, 1);
+            //GameObject world = Instantiate(Butterfly, transform.position, transform.rotation);
+        }
+    }
+
+}
---
+++ Particle System/Assets/backHome.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backHome : MonoBehaviour {
+    GameObject head;
+    public Transform target;
+    public float speed;
+    // Use this for initialization
+    void Start () {
+        head = GameObject.Find("Main Camera");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (head.transform.position.x < 3.5 && head.transform.position.x > 2.5 && head.transform.position.z < 3.5 && head.transform.position.z > 2.5)
+        {
+            Debug.Log("cat");
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            //GameObject world = Instantiate(Butterfly, transform.position, transform.rotation);
+        }
+    }
+}
